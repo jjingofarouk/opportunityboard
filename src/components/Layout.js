349beef68c43fb40870a6551345025531b450c8f
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-16 05:15:35 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import styles from './Layout.module.css';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={styles.layout}>
       <Navbar />
-      <main>{children}</main>
+      <main className={styles.main}>
+        {children}
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
